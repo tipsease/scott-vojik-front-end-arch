50e03ae9bdb513975f56cec7e06d4af9ed6e20e6
Scott Vojik <scott.vojik@gmail.com>
2019-02-03 21:27:12 -0800

    form working, able to add servers

--- tipsease/src/Components/StaffForm/StaffForm.js
+++ tipsease/src/Components/StaffForm/StaffForm.js
@@ -22,6 +22,8 @@ function StaffForm(props) {
 
       <input type="text" name="name" onChange={props.changeHandler} placeholder="Name" value={props.staff.name} />
       <input type="text" name="imgUrl" onChange={props.changeHandler} placeholder="Profile Picture" value={props.staff.imgUrl} />
+      <input type="text" name="price" onChange={props.changeHandler} placeholder="Price" value={props.staff.price} />
+      <input type="text" name="description" onChange={props.changeHandler} placeholder="Description" value={props.staff.description} />
       {/* <input type="text" name="name" onChange={props.changeHandler} placeholder="name" value={props.staff.name} /> */}
 
       <button onClick={handleClick}>Add Staff Member</button>
